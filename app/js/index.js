2968931247f134a109aeeb09a14a3f9a2bfe1ae4
codesleeps <codesleep43@gmail.com>
2023-09-09 20:47:39 +0100

    new design of old app

---
+++ app/js/index.js
@@ -0,0 +1,265 @@
+// const products = [
+  
+//   {
+//     id: 101,
+//     image: "app/img/feco/FECO_SHOTS_CHERRY_LITE-min.png",
+//     alt: "Cherry Syrup",
+//     stars: 5,
+//     name: "Cherry",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 102,
+//     image: "app/img/feco/FECO_SHOTS_FOREST_FRUITS_LITE-min.png",
+//     alt: "Forest Fruits",
+//     stars: 5,
+//     name: "Forest Fruits",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 103,
+//     image: "app/img/feco/FECO_SHOTS_FRUIT_PUNCH_LITE-min.png",
+//     alt: "Fruit Punch",
+//     stars: 5,
+//     name: "Fruit Punch",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 104,
+//     image: "app/img/feco/FECO_SHOTS_GRAPE_LITE-min.png",
+//     alt: "Grape",
+//     stars: 5,
+//     name: "Grape",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 105,
+//     image: "app/img/feco/FECO_SHOTS_IRN-BRU_LITE-min.png",
+//     alt: "Irn-Bru",
+//     stars: 5,
+//     name: "Irn-Bru",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 106,
+//     image: "app/img/feco/FECO_SHOTS_KOLA_CHAMPAGNE_LITE-min.png",
+//     alt: "Kola Champagne",
+//     stars: 5,
+//     name: "Kola Champagne",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 107,
+//     image: "app/img/feco/FECO_SHOTS_MANGO_LITE-min.png",
+//     alt: "Mango",
+//     stars: 5,
+//     name: "Mango",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 108,
+//     image: "app/img/feco/FECO_SHOTS_PINEAPPLE_LITE-min.png",
+//     alt: "Pineapple",
+//     stars: 5,
+//     name: "Pineapple",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 109,
+//     image: "app/img/feco/FECO_SHOTS_RIBENA_LITE-min.png",
+//     alt: "Ribena",
+//     stars: 5,
+//     name: "Ribena",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 110,
+//     image: "app/img/feco/FECO_SHOTS_SORREL&GINGERE_LITE-min.png",
+//     alt: "Sorrel&Ginger",
+//     stars: 5,
+//     name: "Sorrel&Ginger",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 111,
+//     image: "app/img/feco/FECO_SHOTS_STRAWBERRY_LITE-min.png",
+//     alt: "Strawberry Syrup",
+//     stars: 5,
+//     name: "Strawberry",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+//   {
+//     id: 112,
+//     image: "app/img/feco/FECO_SHOTS_VIMTO_LITE-min.png",
+//     alt: "Vimto Syrup",
+//     stars: 5,
+//     name: "Vimto",
+//     sizePrice: [
+//       "50ml - £14.99",
+//       "200ml - £39.99",
+//       "300ml - £49.99",
+//       "500ml - £74.99",
+//       "1000ml - £129.99",
+//     ],
+//     Quantity: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+//   },
+// ];
+
+// let productsHTMl = "";
+
+// products.forEach((product) => {
+//   productsHTMl += `
+//    <div class="col">
+//             <div class="card h-100 border border-3">
+//             <img
+//               src="${product.image}"
+//               class="card-img-top" loading="lazy" width="400" height="267"
+//               alt="${product.alt}"
+//             />
+//             <div class="card-body">
+//               <p class="card-text text-light">
+//                 <a href="#exampleModal" type="button"  data-mdb-toggle="modal" data-mdb-target="#exampleModal">${product.stars}</a><br><br>
+//                 ${product.name} 
+//             <div class="product-size-container">
+              
+//             <select>
+//               <option selected value="1">50ml -  £14.99</option>
+//               <option value="2">200ml - £39.99</option>
+//               <option value="3">300ml - 49.99</option>
+//               <option value="4">500ml - £74.99</option>
+//               <option value="5">1000ml - £129.99</option>  
+//             </select> ${product.sizePrice}
+//           </div><br>
+//          <div class="product-quantity-container">
+                
+//             <select>
+//               <option selected value="1">1</option>
+//               <option value="2">2</option>
+//               <option value="3">3</option>
+//               <option value="4">4</option>
+//               <option value="5">5</option>
+//               <option value="6">6</option>
+//               <option value="7">7</option>
+//               <option value="8">8</option>
+//               <option value="9">9</option>
+//               <option value="10">10</option>
+//             </select> ${product.quantity}
+//           </div><br>
+//               </p>
+//                 <!-- cart button -->
+//                 <button id="feedbackButton" class="button-85" role="button">
+//                   <a href="https://www.youtube.com/watch?v=-ZhxFtmAoXg" target="_blank" class="text-warning">Add to cart</a>
+//                 </button>
+//             </div>
+//           </div>
+//         </div>
+//   `;
+// });
+
+// console.log(productsHTMl);
+
+// document.querySelector('.js-product-grid').innerHTML = productsHTMl;
+
+
+
+
+
+
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
