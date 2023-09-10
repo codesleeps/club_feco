5486ff42334a190f6eb8417acebbcd34d761b00d
codesleeps <codesleep43@gmail.com>
2023-09-10 15:14:24 +0100

    update sw cache first func

--- sw.js
+++ sw.js
@@ -61,3 +61,13 @@ self.addEventListener("install", (event) => {
     })
   );
 });
+// Responding with only cached resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request));
+});
+// Implementing cache first
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request)).then((cachedResponse) => {
+    return cachedResponse || fetch(event.request);
+  });
+});
